77e664f56747bf37964581c3b3e8a73ce58461f9
LimJiNa <jn4624@gmail.com>
2024-03-15 00:34:18 +0900

    Initial commit

---
+++ src/main/java/hello/item/ItemServiceByThymeleafApplication.java
@@ -0,0 +1,13 @@
+package hello.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceByThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceByThymeleafApplication.class, args);
+	}
+
+}
