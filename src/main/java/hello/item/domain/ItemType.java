5d92d98c29fac25b92189c32877210dd0e0617ca
LimJiNa <jn4624@gmail.com>
2024-03-15 23:08:27 +0900

    타임리프 스프링 통합 - 요구사항 추가 (판매 여부, 등록 지역, 상품 종류, 배송 방식)

---
+++ src/main/java/hello/item/domain/ItemType.java
@@ -0,0 +1,15 @@
+package hello.item.domain;
+
+public enum ItemType {
+    BOOK("도서"), FOOD("식품"), ETC("기타");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
